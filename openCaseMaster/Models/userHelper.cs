c68fee2910b9e57834320f61747f6b285b6a5e1a
sunxujianxiou@sina.com <sunxujianxiou@sina.com>
2016-03-07 21:36:28 +0800

    OK

---
+++ openCaseMaster/Models/userHelper.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace openCaseMaster.Models
+{
+    public class userHelper
+    {
+
+        /// <summary>
+        /// 获得ID
+        /// </summary>
+        /// <returns></returns>
+        public static int getUserID()
+        {
+
+            FormsIdentity id = (FormsIdentity)HttpContext.Current.User.Identity;
+            var userData = id.Ticket.UserData;//cookie
+            
+            JObject userJ = JObject.Parse(userData);
+            return Convert.ToInt32(userJ["ID"]);
+        }
+
+        /// <summary>
+        /// 获得项目权限
+        /// </summary>
+        public static string getUserPermission()
+        {
+
+            FormsIdentity id = (FormsIdentity)HttpContext.Current.User.Identity;
+            var userData = id.Ticket.UserData;//cookie
+
+            JObject userJ = JObject.Parse(userData);
+            return userJ["Permission"].ToString().TrimEnd(',');
+        }
+    }
+}
