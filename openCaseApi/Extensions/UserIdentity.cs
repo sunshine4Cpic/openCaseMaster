c84da5120811c9cf46334dfe44e077a5b0a2ebc4
sunxujianxiou@sina.com <sunxujianxiou@sina.com>
2016-07-22 10:03:04 +0800

    修正

---
+++ openCaseApi/Extensions/UserIdentity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace openCaseApi.Extensions
+{
+    public static class UserIdentity
+    {
+
+        public static int userID(this System.Security.Principal.IIdentity _id)
+        {
+
+
+            var cm = (_id as ClaimsIdentity).FindFirst(ClaimTypes.NameIdentifier);
+
+
+            int ID = Convert.ToInt32(cm.Value);
+
+            return ID;
+        }
+
+        public static string userName(this System.Security.Principal.IIdentity _id)
+        {
+            var cm = (_id as ClaimsIdentity).FindFirst("userName");
+
+
+            return cm.Value;
+
+            
+        }
+
+
+    }
+}
